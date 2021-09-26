7b96cac2914b27c3fc4f5026f02fdbe77dbcc3e6
pedrolopesjc <40678796+pedrolopesjc@users.noreply.github.com>
2021-09-26 20:52:55 -0300

    Create HierarchicalClustering.py

---
+++ HierarchicalClustering.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.array([[5,3],
+    [10,15],
+    [15,12],
+    [24,10],
+    [30,30],
+    [85,70],
+    [71,80],
+    [60,78],
+    [70,55],
+    [80,91],])
+
+labels = range(1, 11)
+plt.figure(figsize=(10, 7))
+plt.subplots_adjust(bottom=0.1)
+plt.scatter(X[:,0],X[:,1], label='True Position')
+
+for label, x, y in zip(labels, X[:, 0], X[:, 1]):
+    plt.annotate(
+        label,
+        xy=(x, y), xytext=(-3, 3),
+        textcoords='offset points', ha='right', va='bottom')
+plt.show()
